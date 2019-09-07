92c50f238abe7f46036acf89070b190738eb99d6
Rafael Dana <rafa.rafael02@gmail.com>
2019-09-07 16:19:05 -0300

    0

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-db.collection('produtos').get().then((snapshot) => {
-	console.log(snapshot.docs);
-})
+var firebaseConfig = {
+    apiKey: "AIzaSyBmfC4Oc7QKKy-w0HVTylCjzuBkcfww1RA",
+    authDomain: "lunattic-acd25.firebaseapp.com",
+    databaseURL: "https://lunattic-acd25.firebaseio.com",
+    projectId: "lunattic-acd25",
+    storageBucket: "lunattic-acd25.appspot.com",
+    messagingSenderId: "754179235652",
+    appId: "1:754179235652:web:412b413d897d7bc0074111"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
