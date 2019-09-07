9ec588ae1a755d897297810198c9b76499aa08a3
Rafael Dana <rafa.rafael02@gmail.com>
2019-09-07 16:56:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,50 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+function buy(){
+	var produtosF =[];
+	for (let i = 0; i < produtos.length; i++) {
+		if (produtos[i].cart){
+			var produto = {
+				nome: produtos[i].nome,
+				preco: produtos[i].preco,
+				qnt: produtos[i].qnt,
+				total: produtos[i].total
+			}
+			produtosF.push(produto)
+		}
+
+	}
+	firebase().database().ref('cart').push({
+		total:total(),
+		produtos:produtosF 
+	});
+	clean();
+}
+
+var produtos = [
+	{
+		id:1,
+		img:'./oi1.png',
+		nome:'oi1',
+		preco:3,
+		cart:false,
+		qnt:1,
+		total:0
+	},
+	{
+		id:2,
+		img:'./oi2.png',
+		nome:'oi2',
+		preco:4,
+		cart:false,
+		qnt:1,
+		total:0
+	},
+];
+
+  //RENDER
+  (() =>{
+
+  })();
